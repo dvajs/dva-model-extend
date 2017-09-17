37a3e78d917097fbaffc8dc5bae79d6f4c89d9ab
杨斌 <892728595@qq.com>
2017-09-17 21:13:37 +0800

    增加覆盖信息的提示，只对Object进行检查

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 export default function modelExtend(...models) {
+  const f = (arr, key, count) => {
+    if (arr.indexOf(key) === -1) {
+      arr.push(key);
+    } else {
+      !counts[count] && (counts[count] = {});
+      counts[count][key] ? counts[count][key]++ : counts[count][key] = 1;
+    }
+  }
+  const counts = {};
   const base = {
     state: {},
     subscriptions: {},
     effects: {},
     reducers: {},
   };
-  return models.reduce((acc, extend) => {
+  let stateCache = [],
+    namespaceCache = [],
+    subscriptionsCache = [],
+    effectsCache = [],
+    reducersCache = [];
+
+  const model = models.reduce((acc, extend) => {
+    if (typeof extend.state === 'object' && !Array.isArray(extend.state)) {
+      for (let key in extend.state) {
+        f(stateCache, key, 'stateCount');
+      }
+    }
+    for (let key in extend.subscriptions) {
+      f(subscriptionsCache, key, 'subscriptionsCount');
+    }
+    for (let key in extend.effects) {
+      f(effectsCache, key, 'effectsCount');
+    }
+    for (let key in extend.reducers) {
+      f(reducersCache, key, 'reducersCount');
+    }
     acc.namespace = extend.namespace;
     if (typeof extend.state === 'object' && !Array.isArray(extend.state)) {
       Object.assign(acc.state, extend.state);
@@ -17,4 +46,26 @@ export default function modelExtend(...models) {
     Object.assign(acc.reducers, extend.reducers);
     return acc;
   }, base);
+
+  if (counts['stateCount']) {
+    for (let key in counts['stateCount']) {
+      console.warn(`model: ${model.namespace} state attr: ${key} be overwritten: ${counts['stateCount'][key]} time(s).`);
+    }
+  }
+  if (counts['subscriptionsCount']) {
+    for (let key in counts['subscriptionsCount']) {
+      console.warn(`model: ${model.namespace} subscriptions attr: ${key} be overwritten: ${counts['subscriptionsCount'][key]} time(s).`);
+    }
+  }
+  if (counts['effectsCount']) {
+    for (let key in counts['effectsCount']) {
+      console.warn(`model: ${model.namespace} effects attr: ${key} be overwritten: ${counts['effectsCount'][key]} time(s).`);
+    }
+  }
+  if (counts['reducersCount']) {
+    for (let key in counts['reducersCount']) {
+      console.warn(`model: ${model.namespace} reducers attr: ${key} be overwritten: ${counts['reducersCount'][key]} time(s).`);
+    }
+  }
+  return model;
 };
