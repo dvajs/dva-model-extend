69bfa336142febce0c4109b6e434a8c0d4a30eb1
Benjy Cui <benjytrys@gmail.com>
2017-10-17 12:01:22 +0800

    bump 0.1.2

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
 const check = (origin, cache, count) => {
-  for (let key in origin) {
-    if (cache.indexOf(key) === -1) {
-      cache.push(key);
-    } else {
-      count[key] ? count[key]++ : count[key] = 1;
+  if (process.env.NODE_ENV !== 'production') {
+    for (let key in origin) {
+      if (cache.indexOf(key) === -1) {
+        cache.push(key);
+      } else {
+        count[key] ? count[key]++ : count[key] = 1;
+      }
     }
   }
 }
 
 const log = (model, constitute, count) => {
-  let logCount = 0;
-  for (let key in count) {
-    if (!logCount) {
-      console.warn(`Please note that some of the attributes are inherited in the ${model.namespace} / ${constitute}:`);
+  if (process.env.NODE_ENV !== 'production') {
+    let logCount = 0;
+    for (let key in count) {
+      if (!logCount) {
+        console.warn(`Please note that some of the attributes are inherited in the ${model.namespace} / ${constitute}:`);
+      }
+      logCount++;
+      console.warn(`  -> ${key} be overwritten ${count[key]} time(s).`);
     }
-    logCount++;
-    console.warn(`-> ${key} be overwritten ${count[key]} time(s).`);
   }
 }
 
 export default function modelExtend(...models) {
-  const
-    base = { state: {}, subscriptions: {}, effects: {}, reducers: {}, },
-    stateCache = [], stateCount = {},
-    subscriptionsCache = [], subscriptionsCount = {},
-    effectsCache = [], effectsCount = {},
-    reducersCache = [], reducersCount = {};
+  const base = { state: {}, subscriptions: {}, effects: {}, reducers: {}, };
+  const stateCache = [];
+  const stateCount = {};
+  const subscriptionsCache = [];
+  const subscriptionsCount = {};
+  const effectsCache = [];
+  const effectsCount = {};
+  const reducersCache = [];
+  const reducersCount = {};
 
   const model = models.reduce((acc, extend) => {
-
     acc.namespace = extend.namespace;
     if (typeof extend.state === 'object' && !Array.isArray(extend.state)) {
       check(extend.state, stateCache, stateCount)
